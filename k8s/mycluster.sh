cf19bfed5fa5dca1529e846564c72cc085e20fe5
edXProjectUser-at-498289857405 <godfrey_tutu@yahoo.com>
2021-08-12 09:48:16 +0000

    Added more k8s template

--- k8s/mycluster.sh
+++ k8s/mycluster.sh
@@ -1,9 +1,26 @@
 #! /bin/bash
 
+ZONE=europe-west1-d
+CLUSTER_NAME=my-k8s-cluster
 function create_cluster() {
-	gcloud container clusters create my-k8s-cluster --num-nodes 2 --zone europe-west1-d
+        NUM_NODE=1 
+        if [[ ! -z $1 ]]; then
+          echo $1 is number
+	  NUM_NODE=$1
+        fi
+           
+	gcloud container clusters create $CLUSTER_NAME --num-nodes $NUM_NODE --zone $ZONE
+}
+
+function resize_cluster() {
+ if [[ -z $1 ]]; then
+   echo Please provide the number to resize to.;
+   return 3
+ fi
+ NUM_NODES=$1
+ gcloud container clusters resize   $CLUSTER_NAME --num-nodes=$NUM_NODES --zone $ZONE;
 }
 
 function delete_cluster() {
-	gcloud container clusters delete my-k8s-cluster --zone europe-west1-d 
+	gcloud container clusters delete $CLUSTER_NAME --zone $ZONE 
 }
